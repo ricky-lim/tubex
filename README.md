d6f5d408c0f26161a05f6d6e25aac9136253a64d
ricky-lim <rlim.email@gmail.com>
2020-01-16 16:06:42 +0100

    Prepare for packaging

---
+++ README.md
@@ -0,0 +1,18 @@
+# pytube
+
+Command Line Interface to download and tag MP3 from youtube.
+
+```
+tubex download <youtube_playlist_url> --album <album_name> --artist <artist_name>
+```
+
+## Example
+```bash
+
+$ tubex download https://www.youtube.com/playlist?list=PLSdmnemG6jbv1-GRf23jwDsYWkjz7mblP \
+  --album test --artist mixed
+ 
+# Enjoy the music (e.g using `vlc`)
+$ vlc pytube_out
+```
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    'youtube-dl==2020.1.15',
+    'click==7.0',
+    'eyeD3==0.9'
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tubex",
+    version="0.1",
+    author="Ricky Lim",
+    author_email="rlim.email@gmail.com",
+    description="Command Line Interface to download and tag MP3 from youtube",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ricky-lim/tubex",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    entry_points={"console_scripts": ["tubex=tubex.cli:cli"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
