2234518ca5e2786dcbb264f0339f98667d71585b
ricky-lim <rlim.email@gmail.com>
2020-01-16 16:42:17 +0100

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="tubex",
-    version="0.1",
+    version="0.2",
     author="Ricky Lim",
     author_email="rlim.email@gmail.com",
     description="Command Line Interface to download and tag MP3 from youtube",
