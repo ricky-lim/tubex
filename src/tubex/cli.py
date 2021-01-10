87d5f608e79ac902d0b35ec49182e01e0eff2d3d
rl1m <rlim.email@gmail.com>
2021-01-10 23:35:04 +0100

    Add download mp4 with audio and video with threads

--- src/tubex/cli.py
+++ src/tubex/cli.py
@@ -1,7 +1,10 @@
-import click
-
 from pathlib import Path
+
+import click
+import youtube_dl
 from tubex import mp3
+from tubex.mp4 import MP4
+from tqdm import tqdm
 
 
 class Logger(object):
@@ -22,7 +25,31 @@ class Config:
             "logger": Logger(),
             "restrictfilenames": True,
             "ignoreerrors": True,
+            'progress_hooks': [self.progress_hook],
         }
+        self.bars = {}
+        self.info_log = tqdm(total=0, position=0, bar_format='{desc}')
+
+    def progress_hook(self, d):
+        if d['status'] == 'finished':
+            filename = d['filename'][-10:]
+            bar = self.bars[filename]
+            bar.write(f"{d['filename']} is finished")
+        if d['status'] == 'downloading':
+            filename = d['filename'][-10:]
+            downloaded = d['downloaded_bytes']
+            total = d.get('total_bytes') or d.get('total_bytes_estimate')
+            if downloaded and total:
+                download_status = \
+                    f"{filename} {d.get('_percent_str')} of " \
+                    f"~{d.get('_total_bytes_estimate_str') or d.get('_total_bytes_str')} " \
+                    f"at {d.get('_speed_str')} ETA {d.get('_eta_str')}"
+                if filename not in self.bars:
+                    self.bars[filename] = tqdm(desc=download_status, unit="B",
+                                               unit_scale=True, total=total, leave=True)
+                bar = self.bars[filename]
+                bar.update(downloaded - bar.n)
+                bar.set_description(download_status)
 
 
 pass_config = click.make_pass_decorator(Config, ensure=True)
@@ -72,8 +99,6 @@ def download_mp3(config, url, album, artist):
 @click.argument("url")
 @pass_config
 def download_oreilly(config, url):
-    import youtube_dl
-
     config.opts.update(
         {
             "format": "best[tbr<=1000]/worst[[height>=720]]/best[[height<720]]",
@@ -84,3 +109,10 @@ def download_oreilly(config, url):
         click.echo("Downloading...")
         ydl.download([url])
         exit(0)
+
+
+@cli.command()
+@click.argument("url")
+@pass_config
+def download_mp4(config, url):
+    MP4(config=config, url=url).create()
---
+++ src/tubex/mp4.py
@@ -0,0 +1,60 @@
+import ffmpeg
+import shutil
+from pathlib import Path
+from threading import Thread
+
+import youtube_dl
+
+
+class DownloadThread(Thread):
+    def __init__(self, opts, url):
+        super().__init__()
+        self.opts = opts
+        self.url = url
+
+    def run(self):
+        with youtube_dl.YoutubeDL(self.opts) as ydl:
+            download_info = ydl.extract_info(self.url, download=False)
+            filename = Path(ydl.prepare_filename(download_info))
+            ydl.download([self.url])
+        self.result = filename
+
+
+class MP4:
+    def __init__(self, config, url):
+        self.config = config
+        self.url = url
+        self.audio_opts = dict(self.config.opts, **{
+            "format": "bestaudio[ext=m4a]/bestaudio",
+            "outtmpl": f"{self.config.outdir}/audio/%(title)s.%(ext)s",
+        })
+        self.video_opts = dict(self.config.opts, **{
+            "format": "bestvideo[ext=mp4]/bestvideo",
+            "outtmpl": f"{self.config.outdir}/video/%(title)s.%(ext)s"
+        })
+
+    def merge(self, audio_file, video_file):
+        result = Path(self.config.outdir) / f"{Path(audio_file).stem}.mp4"
+        input_video = ffmpeg.input(video_file)
+        input_audio = ffmpeg.input(audio_file)
+        out = ffmpeg.output(input_video, input_audio, filename=result,
+                            vcodec='copy', acodec='aac', strict='experimental')
+        out.run(quiet=True)
+        return result
+
+    def cleanup(self, video_file, audio_file):
+        shutil.rmtree(Path(video_file).parent)
+        shutil.rmtree(Path(audio_file).parent)
+
+    def create(self):
+        audio_thread = DownloadThread(opts=self.audio_opts, url=self.url)
+        video_thread = DownloadThread(opts=self.video_opts, url=self.url)
+        audio_thread.start()
+        video_thread.start()
+        audio_thread.join()
+        video_thread.join()
+        self.config.info_log.set_description_str(f"Start merging video and audio...")
+        audio_file, video_file = audio_thread.result, video_thread.result
+        res = self.merge(audio_file=audio_file, video_file=video_file)
+        self.cleanup(audio_file=audio_file, video_file=video_file)
+        return res
